a7831eba13bd9f8e9a0039ab4ebca2868bb1dac1
ale-fuentes <67701790+ale-fuentes@users.noreply.github.com>
2020-11-10 14:45:11 -0300

    20201110 - create first MVC files

--- MyFirstExample/Startup.cs
+++ MyFirstExample/Startup.cs
@@ -16,6 +16,7 @@ namespace MyFirstExample
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,24 @@ namespace MyFirstExample
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                 name: "default",
+                 template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //app.Run(async (context) => {
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            //app.UseRouting();
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
         }
     }
 }
